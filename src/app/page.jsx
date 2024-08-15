5d7a3a63400e252c696d7c1193a973c8862aa2ca
Kishore.V <113716171+Master-kishore@users.noreply.github.com>
2024-08-15 17:49:46 +0530

    config the files

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,12 @@
+"use client"
+import CustomButton from "@/components/CustomButton";
+import First from "@/files/First/page";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-    </>
+    <div className="bg-black h-screen">
+        <First />
+    </div>
   );
 }
