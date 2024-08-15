406c5bce85e17b1258fcc6e70dce75ca94d9c9e6
Kishore.V <113716171+Master-kishore@users.noreply.github.com>
2024-08-15 18:39:21 +0530

    problem fix

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,4 @@
 "use client"
-import CustomButton from "@/components/CustomButton";
-import First from "@/files/First/page";
-import Image from "next/image";
 
 export default function Home() {
   return (
