64c92ba9e99133ab8662c6be704c3c01e0ccaeee
Kishore.V <113716171+Master-kishore@users.noreply.github.com>
2024-08-15 21:34:23 +0530

    border added

--- src/app/First.jsx
+++ src/app/First.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function First() {
   return (
-    <div>First</div>
+   <div className="p-5">
+      <div className="border-4 border-white h-[560px] sm:hidden">
+
+      </div>
+
+   </div>
   )
 }
