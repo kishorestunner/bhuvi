533b8242548225c1446319664035b56e98455f22
Kishore.V <113716171+Master-kishore@users.noreply.github.com>
2024-08-18 17:40:29 +0530

    small changes

--- src/app/First.jsx
+++ src/app/First.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function First() {
   return (
-   <div className="p-5">
-      <div className="border-4 border-white h-[750px] sm:hidden">
+    <div className="p-5">
+      <div className="border-4 border-white h-screen sm:hidden">
         <div className="text-center pt-3">
-            <h1 className="text-primary-400 text-8xl uppercase">Happy Birthday</h1>
+          <h1 className="text-primary-400 text-8xl uppercase">Happy Birthday</h1>
         </div>
       </div>
-   </div>
-  )
+    </div>
+  );
 }
