1f160fb8feaf9e082f0f2a8babd936862c4e102c
Kishore.V <113716171+Master-kishore@users.noreply.github.com>
2024-08-18 15:32:21 +0530

    border small changes

--- src/app/First.jsx
+++ src/app/First.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function First() {
   return (
    <div className="p-5">
-      <div className="border-4 border-white h-[560px] sm:hidden">
+      <div className="border-4 border-white h-[750px] sm:hidden">
         <div className="text-center pt-3">
             <h1 className="text-primary-400 text-8xl uppercase">Happy Birthday</h1>
         </div>
